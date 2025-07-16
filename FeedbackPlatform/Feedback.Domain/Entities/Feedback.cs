0baf1063b90f8870176b09c11609f1bac319daef
Pedro Hasse Niemczewski <pedro.hasse@dtidigital.com.br>
2025-07-16 15:56:14 -0300

    Criação de entidades, VOs, Interfaces, EF Core e provedor Postgres, persistência de dados e migrations, com injeção de dependências e swagger funcional.

---
+++ FeedbackPlatform/Feedback.Domain/Entities/Feedback.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using FeedbackApp.Domain.ValueObjects;
+
+namespace FeedbackApp.Domain.Entities
+{
+    public class Feedback
+    {
+        public Guid Id { get; private set; } = Guid.NewGuid();
+        public TextoFeedback Texto { get; private set; }
+        public DateTime DataEnvio { get; private set; } = DateTime.UtcNow;
+
+        public Guid UsuarioId { get; private set; }
+        public Usuario Usuario { get; private set; }
+
+        protected Feedback() { }
+
+        public Feedback(TextoFeedback texto, Guid usuarioId)
+        {
+            Texto = texto ?? throw new ArgumentNullException(nameof(texto));
+
+            if (usuarioId == Guid.Empty)
+                throw new ArgumentException("O ID do usuário não pode ser vazio.", nameof(usuarioId));
+
+            UsuarioId = usuarioId;
+        }
+    }
+}
---
+++ FeedbackPlatform/Feedback.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using FeedbackApp.Domain.Entities;
+
+namespace FeedbackApp.Infrastructure.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Feedback> Feedbacks { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Usuario>(entity =>
+            {
+                entity.OwnsOne(u => u.Email, email =>
+                {
+                    email.Property(e => e.Endereco)
+                        .HasColumnName("Email")
+                        .IsRequired();
+                });
+            });
+
+            modelBuilder.Entity<Feedback>(entity =>
+            {
+                entity.OwnsOne(f => f.Texto, texto =>
+                {
+                    texto.Property(t => t.Texto)
+                        .HasColumnName("Texto")
+                        .IsRequired()
+                        .HasMaxLength(500);
+                });
+
+                entity.HasOne(f => f.Usuario)
+                    .WithMany(u => u.FeedBacksRecebidos)
+                    .HasForeignKey(f => f.UsuarioId)
+                    .OnDelete(DeleteBehavior.Cascade);
+            });
+        }
+    }
+}
