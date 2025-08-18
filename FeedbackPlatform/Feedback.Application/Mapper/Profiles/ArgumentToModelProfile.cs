67dc6093cd7c3f771b138ad9ebdd8856e2fc0f2d
Pedro Hasse Niemczewski <pedro.hasse@dtidigital.com.br>
2025-08-18 15:28:48 -0300

    Adiciona Profiles e configurações do AutoMapper

---
+++ FeedbackPlatform/Feedback.Application/Mapper/Profiles/ArgumentToModelProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using FeedbackApp.Application.Arguments;
+using FeedbackApp.Application.Models;
+
+namespace FeedbackApp.Application.Mapper.Profiles
+{
+    public class ArgumentToModelProfile : Profile
+    {
+        public ArgumentToModelProfile()
+        {
+            CreateMap<UsuarioArgument, UsuarioModel>()
+                .ForMember(dest => dest.SenhaHash, opt => opt.MapFrom(src => src.Senha))
+                .ReverseMap();
+        }
+    }
+}
---
+++ FeedbackPlatform/Feedback.Application/Mapper/Profiles/RequestToModelProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using FeedbackApp.Application.Models;
+using FeedbackApp.Application.Requests;
+
+namespace FeedbackApp.Application.Mapper.Profiles
+{
+    public class RequestToModelProfile : Profile
+    {
+        public RequestToModelProfile()
+        {
+            CreateMap<UsuarioRequest, UsuarioModel>()
+                .ForMember(dest => dest.SenhaHash, opt =>
+                    opt.MapFrom(src => src.Senha))
+                .ForMember(dest => dest.Id, opt => opt.Ignore());
+            //.ForMember(dest => dest.FeedbacksRecebidos, opt => opt.Ignore());
+        }
+    }
+}
