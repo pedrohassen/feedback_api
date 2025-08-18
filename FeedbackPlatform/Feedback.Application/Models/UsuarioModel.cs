0a88b4d0fd19e19faec1904f30806540a550d4af
Pedro Hasse Niemczewski <pedro.hasse@dtidigital.com.br>
2025-08-18 15:25:30 -0300

    Adiciona UsuarioModel

---
+++ FeedbackPlatform/Feedback.Application/Models/UsuarioModel.cs
@@ -0,0 +1,9 @@
+﻿using FeedbackApp.Application.DTOs;
+
+namespace FeedbackApp.Application.Models
+{
+    public class UsuarioModel : UsuarioDTO
+    {
+        public string SenhaHash { get; set; } = string.Empty;
+    }
+}
