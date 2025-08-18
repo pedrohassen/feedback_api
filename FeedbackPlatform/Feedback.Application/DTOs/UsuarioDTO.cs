77ad957d8c57e5d9a556750efd25e7d2b97615c4
Pedro Hasse Niemczewski <pedro.hasse@dtidigital.com.br>
2025-08-18 15:20:52 -0300

    Cria UsuarioDTO, UsuarioArgument, UsuarioRequest, UsuarioResponse e UsuarioModel

---
+++ FeedbackPlatform/Feedback.Application/DTOs/UsuarioDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace FeedbackApp.Application.DTOs
+{
+    public class UsuarioDTO
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Senha { get; set; } = string.Empty;
+        //public ICollection<Feedback> FeedbacksRecebidos { get; set; } = new List<Feedback>();
+    }
+}
