e47a273b3594b2463d0a063d30ffa8fdb2fdc0aa
Pedro Hasse Niemczewski <pedro.hasse@dtidigital.com.br>
2025-08-18 15:01:19 -0300

    Adiciona configurações iniciais, criação e conexão com database

---
+++ FeedbackPlatform/Feedback.Domain/Entities/Usuario.cs
@@ -0,0 +1,10 @@
+﻿namespace FeedbackApp.Domain.Entities
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string SenhaHash { get; set; } = string.Empty;
+    }
+}
---
+++ FeedbackPlatform/Feedback.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using FeedbackApp.Domain.Entities;
+
+namespace FeedbackApp.Infrastructure.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Usuario>(entity =>
+            {
+                entity.Property(u => u.Email)
+                    .IsRequired()
+                    .HasMaxLength(100);
+            });
+        }
+    }
+}
