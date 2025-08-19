de98baba2da0ea670c8c5767106a11062ffcce49
Pedro Hasse Niemczewski <pedro.hasse@dtidigital.com.br>
2025-08-19 15:06:44 -0300

    Criação do FeedbackDTO

---
+++ FeedbackPlatform/Feedback.Application/DTOs/FeedbackDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace FeedbackApp.Application.DTOs
+{
+    public class FeedbackDTO
+    {
+        public int Id { get; set; }
+        public string Texto { get; set; } = string.Empty;
+        public DateTime DataCriacao { get; set; } = DateTime.UtcNow;
+        public DateTime? DataAtualizacao { get; set; }
+        public UsuarioDTO Usuario { get; set; } = null!;
+        public int UsuarioId { get; set; }
+    }
+}
