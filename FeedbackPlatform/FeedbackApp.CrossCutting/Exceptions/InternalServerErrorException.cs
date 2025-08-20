d78899612e49972d64dc4cc1dfd80c2f55fb3061
Pedro Hasse Niemczewski <pedro.hasse@dtidigital.com.br>
2025-08-20 10:24:48 -0300

    Adiciona ao PR middleware de exception handling

---
+++ FeedbackPlatform/FeedbackApp.CrossCutting/Exceptions/InternalServerErrorException.cs
@@ -0,0 +1,16 @@
+﻿using FeedbackApp.CrossCutting.Exceptions.Base;
+
+namespace FeedbackApp.CrossCutting.Exceptions
+{
+    public class InternalServerErrorException : BaseException
+    {
+        public InternalServerErrorException(
+            string[] mensagens,
+            string titulo = "Erro Interno do Servidor",
+            string mensagemLog = null,
+            Exception innerException = null)
+            : base(mensagens, titulo, 500, mensagemLog, innerException)
+        {
+        }
+    }
+}
