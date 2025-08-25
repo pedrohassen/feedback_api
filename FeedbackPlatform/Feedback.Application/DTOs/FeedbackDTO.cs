47e88ce35ca466f6b14f9af1d12e3798e4fefd80
Pedro Hasse Niemczewski <pedro.hasse@dtidigital.com.br>
2025-08-25 15:35:21 -0300

    Implementação de FeedbackDTO e Requests

--- FeedbackPlatform/Feedback.Application/DTOs/FeedbackDTO.cs
+++ FeedbackPlatform/Feedback.Application/DTOs/FeedbackDTO.cs
@@ -6,7 +6,9 @@
         public string Texto { get; set; } = string.Empty;
         public DateTime DataCriacao { get; set; } = DateTime.UtcNow;
         public DateTime? DataAtualizacao { get; set; }
-        public UsuarioDTO Usuario { get; set; } = null!;
-        public int UsuarioId { get; set; }
+        public int DestinatarioId { get; set; }
+        public UsuarioDTO Destinatario { get; set; } = null!;
+        public int RemetenteId { get; set; }
+        public UsuarioDTO Remetente { get; set; } = null!;
     }
 }
---
+++ FeedbackPlatform/Feedback.Application/Requests/FeedbackRequest.cs
@@ -0,0 +1,8 @@
+﻿using FeedbackApp.Application.DTOs;
+
+namespace FeedbackApp.Application.Requests
+{
+    public class FeedbackRequest : FeedbackDTO
+    {
+    }
+}
