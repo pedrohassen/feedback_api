60761d5646bd6b8931e42234293eee9850df679a
Pedro Hasse Niemczewski <pedro.hasse@dtidigital.com.br>
2025-08-26 15:51:36 -0300

    Modifica RequestToArgumentProfile afim de usar o mesmo request para criação e atualização de feedbacks

--- FeedbackPlatform/Feedback.Application/Mapper/Profiles/RequestToArgumentProfile.cs
+++ FeedbackPlatform/Feedback.Application/Mapper/Profiles/RequestToArgumentProfile.cs
@@ -12,9 +12,11 @@ namespace FeedbackApp.Application.Mapper.Profiles
                 .ForMember(dest => dest.Id, opt => opt.Condition((src, dest, srcMember) => srcMember != 0));
 
             CreateMap<FeedbackRequest, FeedbackArgument>()
-                .ForMember(dest => dest.Id, opt => opt.Ignore())
+                .ForMember(dest => dest.Id, opt => opt.Condition((src, dest, srcMember) => srcMember != 0))
                 .ForMember(dest => dest.Destinatario, opt => opt.Ignore())
                 .ForMember(dest => dest.Remetente, opt => opt.Ignore())
+                .ForMember(dest => dest.RemetenteId, opt => opt.Ignore())
+                .ForMember(dest => dest.DestinatarioId, opt => opt.Ignore())
                 .ForMember(dest => dest.DataCriacao, opt => opt.Ignore())
                 .ForMember(dest => dest.DataAtualizacao, opt => opt.Ignore());
         }
