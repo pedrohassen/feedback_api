9b692eaa3912d7d22ff621c22859e6b120fe16ee
Pedro Hasse Niemczewski <pedro.hasse@dtidigital.com.br>
2025-08-28 16:49:00 -0300

    Ajustes nos Profiles

--- FeedbackPlatform/Feedback.Application/Mapper/Profiles/ModelToEntityProfile.cs
+++ FeedbackPlatform/Feedback.Application/Mapper/Profiles/ModelToEntityProfile.cs
@@ -9,28 +9,16 @@ namespace FeedbackApp.Application.Mapper.Profiles
         public ModelToEntityProfile()
         {
             CreateMap<UsuarioModel, Usuario>()
-                .ForMember(dest => dest.Id, opt => opt.Ignore())
                 .ForMember(dest => dest.Senha, opt => opt.Ignore())
                 .ForMember(dest => dest.FeedbacksRecebidos, opt => opt.Ignore())
-                .ForMember(dest => dest.FeedbacksEnviados, opt => opt.Ignore());
-
-            CreateMap<Usuario, UsuarioModel>()
-                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
-                .ForMember(dest => dest.Nome, opt => opt.MapFrom(src => src.Nome))
-                .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Email));
+                .ForMember(dest => dest.FeedbacksEnviados, opt => opt.Ignore())
+                .ReverseMap();
 
             CreateMap<FeedbackModel, Feedback>()
-                .ForMember(dest => dest.Id, opt => opt.Ignore())
-                .ForMember(dest => dest.DataCriacao, opt => opt.Ignore())
-                .ForMember(dest => dest.DataAtualizacao, opt => opt.Ignore())
+                .ForMember(dest => dest.Id, opt => opt.Condition((src, dest, srcMember) => srcMember != 0))
                 .ForMember(dest => dest.Destinatario, opt => opt.Ignore())
-                .ForMember(dest => dest.Remetente, opt => opt.Ignore());
-
-            CreateMap<Feedback, FeedbackModel>()
-                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
-                .ForMember(dest => dest.Texto, opt => opt.MapFrom(src => src.Texto))
-                .ForMember(dest => dest.DataCriacao, opt => opt.MapFrom(src => src.DataCriacao))
-                .ForMember(dest => dest.DataAtualizacao, opt => opt.MapFrom(src => src.DataAtualizacao));
+                .ForMember(dest => dest.Remetente, opt => opt.Ignore())
+                .ReverseMap();
         }
     }
 }
--- FeedbackPlatform/Feedback.Application/Mapper/Profiles/RequestToArgumentProfile.cs
+++ FeedbackPlatform/Feedback.Application/Mapper/Profiles/RequestToArgumentProfile.cs
@@ -13,10 +13,7 @@ namespace FeedbackApp.Application.Mapper.Profiles
                 .ForMember(dest => dest.Status, opt => opt.Ignore());
 
             CreateMap<FeedbackRequest, FeedbackArgument>()
-                .ForMember(dest => dest.Id, opt => opt.Condition((src, dest, srcMember) => srcMember != 0))
-                .ForMember(dest => dest.RemetenteId, opt => opt.Ignore())
-                .ForMember(dest => dest.DestinatarioId, opt => opt.Ignore())
-                .ForMember(dest => dest.DataCriacao, opt => opt.Ignore());
+                .ReverseMap();
         }
     }
 }
